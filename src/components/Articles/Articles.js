fa58abcc3a642fdc6c6974bed5be28b86d995fa9
Ann Hochworter <ann.hochworter@gmail.com>
2023-11-17 15:56:02 -0700

    Render dummy data articles on main view

--- src/components/Articles/Articles.js
+++ src/components/Articles/Articles.js
@@ -1,11 +1,25 @@
 import './Articles.css';
+import Card from '../Card/Card';
 
-function Articles() {
-  return (
-    <div className="articles">
-      <h2>Article Cards Live Here</h2>
-    </div>
-  );
+function Articles({ articles }) {
+  // const [search, setSearch] = useState('');
+
+  const articleCards = articles.map(article => {
+    const sourceName =
+      article.source && article.source.name
+        ? article.source.name
+        : 'Unknown Source';
+    return (
+      <Card
+        key={article.source.name}
+        sourceName={sourceName}
+        title={article.title}
+        description={article.description}
+        image={article.urlToImage}
+      />
+    );
+  });
+  return <div className="articles">{articleCards}</div>;
 }
 
 export default Articles;
--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,12 +1,30 @@
 import './Card.css';
 import PropTypes from 'prop-types';
 
-function Card() {
+function Card({ sourceName, title, description, urlToImage }) {
   return (
     <div className="card-container">
-      <h2>ARTICLE CARDS!</h2>
+      <div className="card">
+        <h2 className="title">{title}</h2>
+        <p className="description">{description}</p>
+        {/* <p className="source-id">{source.id}</p> */}
+        <p className="source-name">{sourceName}</p>
+        <div className="image-container-main">
+          <img
+            src={urlToImage}
+            alt="topic image"
+            className="article-card-img"
+          />
+        </div>
+      </div>
     </div>
   );
 }
 
 export default Card;
+
+Card.propTypes = {
+  id: PropTypes.string,
+  name: PropTypes.string.isRequired,
+  image: PropTypes.string,
+};
