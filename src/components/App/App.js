0b57fa50e249722f2bf82fa7603a25f0764baa0e
Ann Hochworter <ann.hochworter@gmail.com>
2023-11-18 11:21:36 -0700

    Render the selected article container using router

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,7 +1,10 @@
 import { useState } from 'react';
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
 import Header from '../Header/Header';
 import Articles from '../Articles/Articles';
+import SelectedArticleContainer from '../SelectedArticleContainer/SelectedArticleContainer';
+import ErrorComponent from '../ErrorComponent/ErrorComponent';
 
 function App() {
   const dummyData = [
@@ -89,11 +92,39 @@ function App() {
   ];
 
   const [articles, setArticles] = useState(dummyData);
+  const [error, setError] = useState(null);
 
   return (
     <div className="App">
       <Header className="header" />
-      <Articles className="articles" articles={articles} />
+      {error ? (
+        <div className="app-error-container">
+          <ErrorComponent
+            error={error}
+            message="Apologies, we're having trouble loading the page. Please try again later."
+          />
+        </div>
+      ) : (
+        <Routes>
+          <Route
+            path="/"
+            element={<Articles className="articles" articles={articles} />}
+          />
+          <Route
+            path="/article/:title"
+            element={<SelectedArticleContainer />}
+          />
+          <Route
+            path="*"
+            element={
+              <ErrorComponent
+                error={error}
+                message="Apologies, we can't find the requested page. Please hit the 'Home' button above."
+              />
+            }
+          />
+        </Routes>
+      )}
     </div>
   );
 }
