88b20d4610666b705454cd43a7080f2c858988bf
Ann Hochworter <ann.hochworter@gmail.com>
2023-11-22 15:10:03 -0700

    Add Home Link

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,9 +1,39 @@
 import './Footer.css';
+import linkedin from '../../assets/linkedin.png';
+import github from '../../assets/github.png';
 
 function Footer() {
   return (
     <div className="footer">
-      <h2>FOOTER</h2>
+      <div className="title-block">
+        <div className="title-logo">
+          <h2 className="subtitle">Spotlight News</h2>
+        </div>
+        <p className="project-details">
+          Spotlight News is a Turing School Module 4 project built with
+          React.js. Designed and developed by Ann Hochworter.
+        </p>
+      </div>
+      <div className="linkedin-block">
+        <div className="title-icon">
+          <img src={linkedin} className="footer-icon" alt="linkedin-icon" />
+          <h2 className="subtitle">LinkedIn</h2>
+        </div>
+        <p>
+          <a href="https://www.linkedin.com/in/annhochworter/">
+            Ann Hochworter
+          </a>
+        </p>
+      </div>
+      <div className="github-block">
+        <div className="title-icon">
+          <img src={github} className="footer-icon gh" alt="github-icon" />
+          <h2 className="subtitle">GitHub</h2>
+        </div>
+        <p>
+          <a href="https://github.com/AHochworter">github.com/AHochworter</a>
+        </p>
+      </div>
     </div>
   );
 }
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,21 @@
 import './Header.css';
+import { Link, useLocation } from 'react-router-dom';
 
 function Header() {
+  const location = useLocation();
   return (
     <div className="header">
       <h1 className="heading-primary">
         <span className="heading-primary-main">SPOTLIGHT NEWS</span>
         <span className="heading-primary-sub">Today's Top Headlines</span>
       </h1>
+      <div className="home">
+        {location.pathname !== '/' && (
+          <Link className="home-link" to="/">
+            Home
+          </Link>
+        )}
+      </div>
     </div>
   );
 }
