4e81599e0e7cf25680b0d7fa7f47129d8484f98f
Ann Hochworter <ann.hochworter@gmail.com>
2023-11-23 10:44:10 -0700

    Add cypress tests for selected article view

--- cypress/e2e/articleView.cy.js
+++ cypress/e2e/articleView.cy.js
@@ -1,5 +1,56 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+describe('should intercept GET request, load main page and allow user to select an artilce', () => {
+  beforeEach(() => {
+    const apiKey = Cypress.env('REACT_APP_NEWS_API_KEY');
+    cy.log('Setting up API interception');
+    cy.intercept(
+      'GET',
+      `https://newsapi.org/v2/top-headlines?country=us&apiKey=${apiKey}`,
+      {
+        statusCode: 200,
+        fixture: 'articleData',
+      }
+    )
+      .as('getArticles')
+      .log('Visiting the main page');
+    cy.visit('http://localhost:3000');
+    cy.wait('@getArticles').log('API call completed');
+  });
+
+  it('should load the selected article and display the article elements', () => {
+    cy.get('.card').first().click();
+    cy.url(
+      'http://localhost:3000/article/George%20Santos%20announces%20he%20won%E2%80%99t%20seek%20reelection%20following%20damning%20Ethics%20Committee%20report%20-%20CNN'
+    );
+    cy.get('.selected-article-container').should('have.length', 1);
+    cy.get('.selected-card-img').should('exist');
+    cy.get('.selected-article-title').should(
+      'have.text',
+      'George Santos announces he won’t seek reelection following damning Ethics Committee report - CNN'
+    );
+    cy.get('.description').should(
+      'contain',
+      'The House Ethics Committee released its'
+    );
+    cy.get('.published-at').should(
+      'have.text',
+      'Article Published: November 16, 2023'
+    );
+    cy.get('.link-to-full-article')
+      .should('have.attr', 'href')
+      .and(
+        'eq',
+        'https://www.cnn.com/2023/11/16/politics/ethics-committee-releases-santos-report/index.html'
+      );
+    cy.get('.footer').should('contain', 'Spotlight News');
+    cy.get('.linkedin-block')
+      .find('a')
+      .invoke('attr', 'href')
+      .should('eq', 'https://www.linkedin.com/in/annhochworter/');
+    cy.get('.github-block')
+      .find('a')
+      .invoke('attr', 'href')
+      .should('eq', 'https://github.com/AHochworter');
+    cy.get('.home-link').click();
+    cy.url('eq', 'http://localhost:3000');
+  });
+});
--- cypress/e2e/mainView.cy.js
+++ cypress/e2e/mainView.cy.js
@@ -1,4 +1,4 @@
-describe('should load main page', () => {
+describe('should intercept GET and load main page', () => {
   beforeEach(() => {
     const apiKey = Cypress.env('REACT_APP_NEWS_API_KEY');
     cy.log('Setting up API interception');
@@ -16,7 +16,34 @@ describe('should load main page', () => {
     cy.wait('@getArticles').log('API call completed');
   });
 
-  it('should load main article elements', () => {
+  it('should load main article cards and elements', () => {
     cy.get('h1').should('contain', 'SPOTLIGHT NEWS');
+    cy.get('.search-form').should('exist');
+    cy.get('.article-cards').should('have.length', 1);
+    cy.get('.card').first().should('contain', 'CNN');
+    cy.get('.card')
+      .first()
+      .find('.title')
+      .should(
+        'contain',
+        'George Santos announces he won’t seek reelection following damning Ethics Committee report - CNN'
+      );
+    cy.get('.card').last().should('contain', 'ESPN');
+    cy.get('.card')
+      .last()
+      .find('.title')
+      .should(
+        'contain',
+        "Michigan's Jim Harbaugh accepts Big Ten's 3-game suspension - ESPN"
+      );
+    cy.get('.footer').should('contain', 'Spotlight News');
+    cy.get('.linkedin-block')
+      .find('a')
+      .invoke('attr', 'href')
+      .should('eq', 'https://www.linkedin.com/in/annhochworter/');
+    cy.get('.github-block')
+      .find('a')
+      .invoke('attr', 'href')
+      .should('eq', 'https://github.com/AHochworter');
   });
 });
